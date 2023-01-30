3e95e8ad6426a963c4d9dc3e6199206a388ffd37
tianxuefeng <tianxuefeng2010@gmail.com>
2023-01-30 23:27:06 +0800

    不需要版本号给ChatSDK

---
+++ Example/Pods/TeneasyChatSDKUI_iOS/README.md
@@ -0,0 +1,30 @@
+# TeneasyChatSDKUI_iOS
+
+[![CI Status](https://img.shields.io/travis/7938813/TeneasyChatSDKUI_iOS.svg?style=flat)](https://travis-ci.org/7938813/TeneasyChatSDKUI_iOS)
+[![Version](https://img.shields.io/cocoapods/v/TeneasyChatSDKUI_iOS.svg?style=flat)](https://cocoapods.org/pods/TeneasyChatSDKUI_iOS)
+[![License](https://img.shields.io/cocoapods/l/TeneasyChatSDKUI_iOS.svg?style=flat)](https://cocoapods.org/pods/TeneasyChatSDKUI_iOS)
+[![Platform](https://img.shields.io/cocoapods/p/TeneasyChatSDKUI_iOS.svg?style=flat)](https://cocoapods.org/pods/TeneasyChatSDKUI_iOS)
+
+## Example
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+## Requirements
+
+## Installation
+
+TeneasyChatSDKUI_iOS is available through [CocoaPods](https://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod 'TeneasyChatSDKUI_iOS'
+```
+
+## Author
+
+7938813, tianxuefeng2010@gmail.com
+
+## License
+
+TeneasyChatSDKUI_iOS is available under the MIT license. See the LICENSE file for more info.
+# TeneasyChatSdkUI_iOS-
---
+++ Example/Pods/TeneasyChatSDKUI_iOS/TeneasyChatSDKUI_iOS/Classes/ChatViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ViewController.swift
+//  TeneasyChatSDK_iOS
+//
+//  Created by XiaoFu on 01/19/2023.
+//  Copyright (c) 2023 XiaoFu. All rights reserved.
+//
+
+import UIKit
+import TeneasyChatSDK_iOS
+import SnapKit
+
+
+open class ChatViewController: UIViewController, teneasySDKDelegate {
+
+    lazy var tvChatView: UITextView = {
+        let tv = UITextView()
+        return tv
+    }()
+    
+    
+    var lib = ChatLib()
+ public func receivedMsg(msg: CommonMessage) {
+        print(msg)
+       
+        switch msg.payload{
+        case .content(msg.content):
+            print("text")
+            appendMsg(msg: msg.content.data)
+        case .image(msg.image):
+            print(msg.image)
+            appendMsg(msg: "图片：" + msg.image.uri)
+        case .video(msg.video):
+            print("video")
+        case .audio(msg.audio):
+            print("audio")
+        default:
+            print("ddd")
+        }
+    }
+    
+    public   func connected(c: Bool) {
+        if c == true{
+            tvChatView.text.append("\n已连接上！\n\n")
+        }else{
+            //tvChatView.text.append("\n已断开连接\n\n")
+            //tvChatView.text.append("\n重新连接\n")
+            initSDK()
+        }
+    }
+    
+    public  func msgReceipt(msg: CommonMessage){
+        tvChatView.text.append("                       " +  msg.content.data)
+        if msg.msgID == 0{
+            tvChatView.text.append("                    发送失败")
+        }
+      
+        tvChatView.text.append("                                         " +  Date().getFormattedDate(format: "HH:mm:ss") + "\n\n")
+    }
+    
+    public  func systemMsg(msg: String){
+        appendMsg(msg: msg)
+    }
+    
+    public  func appendMsg(msg: String){
+        tvChatView.text.append(msg + "\n" +  Date().getFormattedDate(format: "HH:mm:ss") + "\n\n")
+        //cpf
+    }
+    
+   
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(tvChatView)
+        tvChatView.snp.makeConstraints { make in
+            make.left.equalTo(10)
+            make.top.equalTo(50)
+            make.trailing.equalToSuperview().offset(-20)
+            make.bottom.equalToSuperview().offset(-50)
+        }
+        
+        tvChatView.text = "teneasy chat sdk 初始化"
+        tvChatView.isUserInteractionEnabled = true
+        tvChatView.isScrollEnabled = true
+        
+        initSDK()
+    }
+    
+    func initSDK(){
+        //从网页端把chatId和token传进sdk,
+        lib = ChatLib(chatId: 2692944494601, token: "CCcQARgJICIon6WAieAw.4hI0uHGRO_-CSlDFnI4036IVnXr7No1wF9f32TCvDXFj27Ph4migozWYC6348C3bvvM-kbdZDAqSNIG2BiAYDw")
+        lib.callWebsocket()
+        lib.delegate = self
+    }
+    
+    public override func viewDidAppear(_ animated: Bool) {
+        let btSend = UIButton()
+        btSend.frame = CGRect(x: 100, y: 600, width: 200, height: 200)
+        btSend.setTitleColor(UIColor.systemRed, for: UIControlState.normal)
+        btSend.setTitle("Send", for: UIControlState.normal)
+        self.view.addSubview(btSend)
+        btSend.addTarget(self, action:#selector(btSendAction), for:.touchUpInside)
+    }
+    
+    @objc func btSendAction(){
+        let txtMsg = "你好！需要什么帮助？\n"
+        lib.sendMessage(msg: txtMsg)
+        let c = CommonMessage()
+        print(c.content)
+    }
+    
+
+    open override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
