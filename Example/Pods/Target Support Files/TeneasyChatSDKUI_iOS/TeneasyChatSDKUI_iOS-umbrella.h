586ee833c8ffeba260d807eeacbaef79b83c3f70
tianxuefeng <tianxuefeng2010@gmail.com>
2023-02-02 23:04:53 +0800

    把图片放都Asset里面

--- Example/Pods/Target Support Files/TeneasyChatSDKUI_iOS/TeneasyChatSDKUI_iOS-umbrella.h
+++ Example/Pods/Target Support Files/TeneasyChatSDKUI_iOS/TeneasyChatSDKUI_iOS-umbrella.h
@@ -10,6 +10,7 @@
 #endif
 #endif
 
+#import "TeneasyChatSDKUI_iOS-TeneasyChatSDKUI_iOS-Bridging-Header.h"
 
 FOUNDATION_EXPORT double TeneasyChatSDKUI_iOSVersionNumber;
 FOUNDATION_EXPORT const unsigned char TeneasyChatSDKUI_iOSVersionString[];
