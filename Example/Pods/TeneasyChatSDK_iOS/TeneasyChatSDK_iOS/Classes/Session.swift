9ce7ffae37a3d69b6d42f6ec61b89c4383c4ad18
tianxuefeng <tianxuefeng2010@gmail.com>
2023-02-05 08:37:52 +0800

    add session.swift

---
+++ Example/Pods/TeneasyChatSDK_iOS/TeneasyChatSDK_iOS/Classes/Session.swift
@@ -0,0 +1,17 @@
+//
+//  PayLoadExt.swift
+//  TeneasyChatSDK_iOS
+//
+//  Created by tian molin on 4/2/23.
+//
+
+import Foundation
+
+public struct Session{
+    var ID : UInt64? = 0//会用连接成功之后返回的第一个PayLoadId作为ID
+    var ChatId = 0
+    var WorkerId = 0
+    var TokenId = 0
+    var WelcomeMsg = "你好，我是客服小福"
+    var Connected = false
+}
